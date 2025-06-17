42821cab8a765750efd9af0655df2fa74051ca5a
2dawg <166908120+2dawg@users.noreply.github.com>
2025-06-17 09:26:39 +0700

    Update ok.lua

--- ok.lua
+++ ok.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local UIS = game:GetService("UserInputService")
-local RunService = game:GetService("RunService")Add commentMore actions
+local RunService = game:GetService("RunService")
 local Workspace = game:GetService("Workspace")
 
 -- GUI
